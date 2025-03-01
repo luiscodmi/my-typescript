d80c973c34a072dc708b5b4692433fa657946550
Luisito <luiscodmi@gmail.com>
2025-03-01 15:11:37 -0700

    incluyendo el .gitignore; y unas modificaciones en la tarea

--- src/hacer.ts
+++ src/hacer.ts
@@ -17,14 +17,25 @@ class List {
     };
 
     this.tasks.push(newTask);
+    console.log(`Nueva tarea agragada: ${title}`);
   }
 
-  editButton(task: Tasks, newTitle: string, newDescription: string) {
-    return {
-      ...task,
-      title: newTitle,
-      description: newDescription,
-    };
+  editButton(
+    id: string,
+    task: Tasks,
+    newTitle: string,
+    newDescription: string
+  ) {
+    var editTask: void[] = this.tasks.map((ed, i) => {
+      if (ed.id === id) {
+        this.tasks[i] = {
+          ...task,
+          id,
+          title: newTitle,
+          description: newDescription,
+        };
+      }
+    });
   }
 
   deleteButton(id: string) {
@@ -40,18 +51,39 @@ class List {
       console.log("No hay tareas en la lista");
     } else {
       console.log("Lista de Tareas");
+      console.log(this.tasks);
+    }
+  }
+
+  completeTask(id: string) {
+    const task = this.tasks.find((t) => t.id === id);
+
+    if (task) {
+      task.complete = true;
+      console.log(`La tarea ${task.title} ha sido comlpetada!`);
     }
   }
 }
 
 const listar = new List();
 
+console.log(listar.listTasks());
 console.log(
-  listar.addTask("1ab2c3", "Mi tarea", "Tengo que volver a estudiar los cursos")
+  listar.addTask(
+    "1ab2c3",
+    "Mi tarea",
+    "Tengo que volver a estudiar los cursos"
+  ),
+  listar.addTask("aaa", "Repaso", "atencion a deficientes"),
+  listar.addTask("bbz", "compra", "buscar los mandados")
 );
 
-console.log(listar.tasks);
+// console.log(listar.tasks);
 
-console.log(listar.editButton(listar.tasks[0], "otra cosa", "hay que repasar"));
+console.log(
+  listar.editButton("aaa", listar.tasks[0], "otra cosa", "hay que repasar")
+);
 
 console.log(listar.listTasks());
+
+listar.completeTask("bbb");
